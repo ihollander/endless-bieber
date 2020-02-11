3978dd2053f7cf8d7e9b6d64bd6496409a7a907d
Ian Hollander <ianholla@gmail.com>
2020-02-11 17:25:51 -0500

    assets url

--- src/Obstacle.js
+++ src/Obstacle.js
@@ -4,7 +4,7 @@ class Obstacle {
     this.coords = coords
 
     this.image = new Image()
-    this.image.src = "endless-bieber/assets/biebs.jpeg"
+    this.image.src = "assets/biebs.jpeg"
   }
 
   draw() {
